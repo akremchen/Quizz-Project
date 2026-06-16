35795dbd805cfc005e1bb3a011af1d4d95a3e9ce
akrem.chen <akrem.chen@gmail.com>
2026-06-16 18:36:35 +0200

    Fix folder paths

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.quizz"
 version = "0.0.1-SNAPSHOT"
-description = "user service"
+description = "user-service"
 
 java {
     toolchain {
@@ -20,13 +20,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
